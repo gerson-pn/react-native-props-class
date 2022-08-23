1a097bfc2638871f1fdbd8b79beb31f3ffe45934
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2022-08-23 15:50:34 -0300

    Inclusão do projeto que mostra como utilizar o objeto state para aplicar mudança de estado em um componente

--- App.js
+++ App.js
@@ -5,11 +5,18 @@ class App extends Component {
   state = {
     data: new Date()
   }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        data: new Date()
+      })
+    }, 1000);
+  }
   render() {
     return (
       <View>
         <Text>Data e hora local:</Text>
-        <Text>{this.state.data.toLocaleDateString()}</Text>
+        <Text>{this.state.data.toLocaleTimeString()}</Text>
       </View>
     )
   }
