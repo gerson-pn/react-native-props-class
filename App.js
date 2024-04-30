1a12cd222534dd71232a1f01c0bb33a6ab170867
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2024-04-30 10:22:13 -0300

    Alteração das propriedades e caminhos de imagens

--- App.js
+++ App.js
@@ -3,20 +3,21 @@ import Cartao from './Cartao';
 
 export default class App extends Component {
   urls = [
-    'https://fatecsjc-prd.azurewebsites.net/images/team/docentes/reinaldo_gen_ichiro_arakaki.jpg',
-    'http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4164443U3',
-    'https://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4767313D7',
-    'https://fatecsjc-prd.azurewebsites.net/images/team/docentes/gerson_da_penha_neto.jpg'
+    'https://sjc.fatec.sp.gov.br/static/media/reinaldo_gen_ichiro_arakaki.dc8cbdc8.jpg',
+    'https://sjc.fatec.sp.gov.br/static/media/claudio_etelvino.a45b8a36.png',
+    'https://sjc.fatec.sp.gov.br/static/media/fernando_masanori.a8ab3abc.png',
+    'https://sjc.fatec.sp.gov.br/static/media/eduardo_sakaue.356eb2a7.jpg'
   ]
   render() {
     return (
       <>
-        <Cartao url={this.urls[0]} nome='Reinaldo Arakake' disciplina='Matemática para Computação' />
-        <Cartao url={this.urls[1]} nome='Cláudio Lima' disciplina='Engenharia de Software' />
+        <Cartao url={this.urls[0]} nome='Reinaldo Arakaki' disciplina='Matematica para Computacao' />
+        <Cartao url={this.urls[1]} nome='Claudio Lima' disciplina='Engenharia de Software' />
         <Cartao url={this.urls[2]} nome='Fernando Masanori' disciplina='Algoritmos' />
-        <Cartao url={this.urls[3]} nome='Gerson Penha' disciplina='Programação Orientada à Objetos' />
+        <Cartao url={this.urls[3]} nome='Eduardo Sakaue' disciplina='Gestao Agil de Projetos' />
       </>
     )
   }
 }
+
 
